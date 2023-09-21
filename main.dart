443df4ba42b5e8429f6ac9fc12e46311c0a148ee
ALI-tech-tech <73696254+ALI-tech-tech@users.noreply.github.com>
2023-09-21 21:00:21 +0300

    DartOOPEvaluationExam part2

--- main.dart
+++ main.dart
@@ -9,11 +9,12 @@ import 'saerch.dart';
 import 'suppliers.dart';
 
 void main(List<String> args) {
-  Search search=Search<Medicine>();
+  Search searchMedicine=Search<Medicine>();
+  Search searchClient=Search<Client>();
   List<Suppliers>sups=List.generate(5, (index) => Suppliers(name: "Suplier${index}", mobile: "77${genratenum()}", company: "Company ${index}"));
   List<Client> clints=List.generate(5, (index) => Client(name: "Client ${index}", mobile: "77${genratenum()}", address: "address ${index}", BirthDate: "${Random().nextInt(30)}/${Random().nextInt(12)}/${Random().nextInt(2023)}"));
   List<Pharmacist> pharm=List.generate(5, (index) => Pharmacist(name: "Pharmacist ${index}", mobile: "77${genratenum()}",salsry: Random().nextInt(10000)));
-  List<Medicine> medicines=List.generate(5, (index) => Medicine(name: "Medicine ${index}", unitprice: Random().nextInt(5000), cat: "Category ${index}", expireDate: "${Random().nextInt(30)}/${Random().nextInt(12)}/${Random().nextInt(2023)}", sup: sups[index]));
+  List<Medicine> medicines=List.generate(5, (index) => Medicine(name: "Medicine ${index}", unitprice: Random().nextInt(100000), cat: "Category ${index}", expireDate: "${Random().nextInt(30)}/${Random().nextInt(12)}/${Random().nextInt(2023)}", sup: sups[index]));
   Invoice iv=Invoice();
   Invoicedetails invdt=Invoicedetails(md2: medicines[0], Qty: 3);
   Invoicedetails invdt2=Invoicedetails(md2: medicines[1], Qty: 1);
@@ -42,15 +43,15 @@ iv.invoDetail.forEach((element) {
     element.printInvoiceDetails();
   });
   print("--------------------->4000-------------------");
- search.search(medicines, 4000 ,operatorT: ">");
+ searchMedicine.search(medicines, 4000.0 ,operatorT: ">");
  print("---------------------category 4-------------------");
- search.search(medicines, "category 4",operatorT: "==");
+ searchMedicine.search(medicines, "Category 4",operatorT: "==");
   print("---------------------Search for Medicines that contains 4 in their name-------------------");
-search.search(medicines, "4") ;
+searchMedicine.search(medicines, "4") ;
  print("---------------------Search for invoices for client 1 and with 1Medicine 1 name-------------------");
-search.search(clints, "client 1",operatorT: "Medicine 1");
+searchClient.search(clints, "Client 1",operatorT: "Medicine 1");
  print("---------------------Get the first 3 medicines with expire date after 2000-------------------");
-search.search(medicines, 2000);
+searchMedicine.search(medicines, 2000);
 
 
 
--- saerch.dart
+++ saerch.dart
@@ -7,36 +7,41 @@ import 'invoice_details.dart';
 class Search<T> {
   
   search(List<T> list,dynamic keyword,{String? operatorT}){
-    if (T.runtimeType.toString()=="Medicine" && keyword.runtimeType.toString() == "double" && operatorT==">") {
+    if (T==Medicine && keyword.runtimeType.toString() == "double" && operatorT==">") {
+      
       list.forEach((element ) {
         if ((element as Medicine).getUnitPrice>keyword) {
           (element as Medicine).display();
         }
       });
-    }else if(T==Medicine && keyword is String && operatorT=="==") {
+    }else if(T==Medicine && keyword.runtimeType.toString() == "String" && operatorT=="==") {
       list.forEach((element ) {
         if ((element as Medicine).category==keyword) {
           (element as Medicine).display();
         }
       });
-    }else if(T==Medicine && keyword is String && keyword.length==1&& operatorT==null) {
+    }else if(T==Medicine && keyword.runtimeType.toString() == "String" && keyword.length==1&& operatorT==null) {
       list.forEach((element ) {
         if ((element as Medicine).name.toString().contains(keyword)) {
           (element as Medicine).display();
         }
       });}
-    if(T is Client && keyword is String && operatorT is String){
+    if(T == Client && keyword.runtimeType.toString() == "String" && operatorT.runtimeType.toString() == "String"){
+      
       list.forEach((element) {
         if((element as Client).name==keyword ){
+          
           (element as Client).invoices.forEach((element) {
+            print("inside client");
             if((element.invoDetail as Invoicedetails).md.name==operatorT){
+              
              (element.invoDetail as Invoicedetails).printInvoiceDetails();
             }
            });
         }
       });
     }
-    if (T is Medicine && keyword is int) {
+    if (T == Medicine && keyword.runtimeType.toString()== "int") {
       List<Medicine> li=[];
       list.forEach((element) {
         List<String> date=(element as Medicine).expireDate.split("/");
