60b26b679318509e511e0317cc853e8a17b0e492
ALI-tech-tech <73696254+ALI-tech-tech@users.noreply.github.com>
2023-10-04 08:40:57 +0300

    Day25 part2

--- lib/MyCard.dart
+++ lib/MyCard.dart
@@ -14,29 +14,48 @@ class MyCard extends StatelessWidget {
       child: Container(
         
         decoration: BoxDecoration(
-         
-          color: Colors.amber,
-          borderRadius: BorderRadius.circular(10)
+          boxShadow:[
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 3), // changes position of shadow
+      ),
+    ] ,
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20)
         ),
-        width: 400,
-        padding: EdgeInsets.all(10),
+        width: 370,
+        height: 90,
         child: Row(
+          
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             MyContainer(),
-            Column(
-              children: [
-                OneRow("Name : ${this.name}"),
-                OneRow("Job : ${this.job}"),
-                OneRow("Major : : ${this.major}"),
-              ],
+            Container(
+              padding: EdgeInsets.all(7),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OneRow("Name : ${this.name}"),
+                  OneRow("Job : ${this.job}"),
+                  OneRow("Major : ${this.major}"),
+                ],
+              ),
             ),
-            ElevatedButton(
-                onPressed: () {},
-                child: Row(
-                  
-                  children: [Text("Add"), Icon(Icons.add)],
-                ))
+            Column(
+              
+             mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      
+                      children: [Text("Add"), Icon(Icons.add)],
+                    )),
+              ],
+            )
           ],
         ),
       ),
