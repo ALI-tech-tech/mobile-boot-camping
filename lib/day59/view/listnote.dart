0f071c59345d6d4982ef6cef1265dd6b518e385b
ALI-tech-tech <73696254+ALI-tech-tech@users.noreply.github.com>
2023-11-21 14:42:01 +0300

    note app with hive

--- lib/day59/view/listnote.dart
+++ lib/day59/view/listnote.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
-
 import '../model/note.dart';
-
 
 class NoteListScreen extends StatefulWidget {
   @override
@@ -12,8 +10,8 @@ class NoteListScreen extends StatefulWidget {
 
 class _NoteListScreenState extends State<NoteListScreen> {
   late Box<Note> noteBox;
-  TextEditingController _titleController=TextEditingController();
-  TextEditingController _contentController=TextEditingController();
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
   @override
   void initState() {
     super.initState();
@@ -34,79 +32,73 @@ class _NoteListScreenState extends State<NoteListScreen> {
             title: Text(note.title),
             subtitle: Text(note.content),
             trailing: IconButton(
-              icon: Icon(Icons.check, color: noteBox.getAt(index)!.isCompleted!?Colors.green:Colors.red,),
-              onPressed: ()async {
-                bool?v= await 
-                showDialog(context: context, builder: (context) {
-                  return AlertDialog(
-                    content: ElevatedButton(onPressed: (){
-                        Note n;
-                n= noteBox.getAt(index)!;
-                n.isCompleted=true;
-                Navigator.pop(context,true);
-                      }, child: Text("Mark as Complete")),
-                    
-                  );
-                  
+              icon: Icon(
+                Icons.check,
+                color: note.isCompleted! ? Colors.green : Colors.red,
+              ),
+              onPressed: () async {
+                bool? v = await showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: ElevatedButton(
+                            onPressed: () {
+                              note.isCompleted = true;
+                              noteBox.putAt(index, note);
+                              Navigator.pop(context, true);
+                            },
+                            child: Text("Mark as Complete")),
+                      );
+                    });
+                if (v != null && v) {
+                  setState(() {});
                 }
-                
-              );
-                if(v!=null && v){
-                setState(() {
-                  
-                });
-              }
-                
               },
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async{
+        onPressed: () async {
           _contentController.clear();
           _titleController.clear();
-          bool? v= await 
-          showDialog(context: context, builder: (context) {
-            return AlertDialog(
-              content: Form(child: Column(
-                mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(labelText: 'Title'),
-            ),
-            TextField(
-              controller: _contentController,
-              decoration: InputDecoration(labelText: 'Content'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                
-                final newNote = Note(
-                  title: _titleController.text,
-                  content: _contentController.text,
-                
-                );
-                noteBox.add(newNote);
+          bool? v = await showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        controller: _titleController,
+                        decoration: InputDecoration(labelText: 'Title'),
+                      ),
+                      TextField(
+                        controller: _contentController,
+                        decoration: InputDecoration(labelText: 'Content'),
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          final newNote = Note(
+                            title: _titleController.text,
+                            content: _contentController.text,
+                          );
+                          noteBox.add(newNote);
 
-                
-                Navigator.pop(context, true);
-              },
-              child: Text('Save Note'),
-            ),
-          ],
-        ),),
-            );
-            
-          } 
-          
-          ,);
-        if (v!=null && v) {
-          setState(() {
-            
-          });
+                          Navigator.pop(context, true);
+                        },
+                        child: Text('Save Note'),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+          if (v != null && v) {
+            setState(() {});
           }
         },
         child: Icon(Icons.add),
