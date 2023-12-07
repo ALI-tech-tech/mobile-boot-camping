aa8a581fa27f1a4c616a1c8a9be2ce99d087c66a
ALI-tech-tech <73696254+ALI-tech-tech@users.noreply.github.com>
2023-12-07 12:51:04 +0300

    tables with relation

---
+++ app/Models/Course.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'hours'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class)->withPivot('grade');
+    }
+}
